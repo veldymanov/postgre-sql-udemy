d56eaff21005524ca7d840b734ebe632e7ada9fc
Andrii Veldymanov <Andrii_Veldymanov@epam.com>
2020-03-30 12:20:37 +0300

    create movie_data, test_db, owners_pets

---
+++ owners_pets/create_db.sql
@@ -0,0 +1,48 @@
+-- Database: owners_pets
+
+-- DROP DATABASE owners_pets;
+
+CREATE DATABASE owners_pets
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1251'
+    LC_CTYPE = 'English_United States.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+		
+-- create owners table
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	city VARCHAR(30),
+	state CHAR(2)
+);
+
+SELECT * FROM owners;
+
+-- create owners table
+
+CREATE TABLE pets (
+	id SERIAL PRIMARY KEY,
+	species VARCHAR(30),
+	full_name VARCHAR(30),
+	age INT,
+	owner_id INT REFERENCES owners (id)
+);
+
+SELECT * FROM pets;
+
+-- add email column to the owners table
+ALTER TABLE owners
+ADD column email VARCHAR(30) UNIQUE;
+
+SELECT * FROM owners;
+
+-- change email column type in the owners table
+ALTER TABLE owners
+ALTER COLUMN email TYPE VARCHAR(50);
+
+
---
+++ test_db/create_db.sql
@@ -0,0 +1,51 @@
+-- Database: test
+
+-- DROP DATABASE test;
+
+CREATE DATABASE test
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1251'
+    LC_CTYPE = 'English_United States.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+		
+-- modify tables and add a column
+
+CREATE TABLE examples (
+	example_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30)
+);
+
+SELECT* FROM examples;
+
+ALTER TABLE examples
+ADD column email VARCHAR(50) UNIQUE;
+
+ALTER TABLE examples
+ADD column nationality VARCHAR(30),
+ADD column age INT NOT NULL;
+
+ALTER TABLE examples
+ALTER column nationality TYPE CHAR(3);
+
+ALTER TABLE examples
+ALTER column last_name TYPE VARCHAR(30),
+ALTER column email TYPE VARCHAR(50);
+
+-- deleting tables from db
+CREATE TABLE practise (
+	id SERIAL PRIMARY KEY,
+	products_name VARCHAR(50),
+	product_price NUMERIC(4, 2)
+);
+
+SELECT * FROM practise;
+
+DROP TABLE practise;
+
+
+
+
